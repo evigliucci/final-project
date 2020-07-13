b6d017096c78d470bcf9c87185a4827d40311c44
Erika Vigliucci <evigliucci@gmail.com>
2020-07-12 21:49:18 -0400

    build routes and temp pages with scss

---
+++ thepeoplespurse/client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/home.css";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>HOMEPAGE</h1>
+    </main>
+  );
+};
+
+export default Home;
