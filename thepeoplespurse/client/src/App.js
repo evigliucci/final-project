4d45398ba1bc7f615a54e74b77fe78b89b37f985
Samuel Thompson <thomps9012@gmail.com>
2020-07-15 09:52:57 -0400

    consolidated navbar into single component

--- thepeoplespurse/client/src/App.js
+++ thepeoplespurse/client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BudgetResults from "./pages/BudgetResults";
 import BudgetVoting from "./pages/BudgetVoting";
+import Navbar from "./components/Navbar/Navbar.js"
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Route exact path="/" component={Home} />
       <Route exact path="/BudgetResults" component={BudgetResults} />
       <Route exact path="/BudgetVoting" component={BudgetVoting} />
