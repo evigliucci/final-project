15a4761688dbe6de203293f1b8433e7feaadec72
Samuel Thompson <thomps9012@gmail.com>
2020-07-16 20:46:15 -0400

    renamed budget slider and unweightedbudget

--- thepeoplespurse/client/src/pages/BudgetResults.js
+++ thepeoplespurse/client/src/pages/BudgetResults.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import "../assets/styles/budgetResults.css";
-import UnweightedBudget from "../components/SuggestedBudgets/unweightedBudget.js";
-import BudgetSlider from "../components/BudgetSlider/budgetSlider";
+import UnweightedBudget from "../components/SuggestedBudgets/UnweightedBudget.js";
+import BudgetSlider from "../components/BudgetSlider/BudgetSlider";
 // import BudgetBtns from "../components/BudgetBtns/BudgetBtns";
 
 const BudgetResults = () => {
