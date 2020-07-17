8be21303bac03beb7d762541f585ea70304fa684
Erika Vigliucci <evigliucci@gmail.com>
2020-07-16 20:37:32 -0400

    APP Bug Fixes

--- thepeoplespurse/client/src/pages/BudgetResults.js
+++ thepeoplespurse/client/src/pages/BudgetResults.js
@@ -1,14 +1,13 @@
 import React from "react";
 // import "../assets/styles/budgetResults.css";
-import UnweightedBudget from "../components/SuggestedBudgets/UnweightedBudget.js";
-import BudgetSlider from "../components/BudgetSlider/BudgetSlider";
+import UnweightedBudget from "../components/SuggestedBudgets/unweightedBudget.js";
+import BudgetSlider from "../components/BudgetSlider/budgetSlider";
 // import BudgetBtns from "../components/BudgetBtns/BudgetBtns";
 
 const BudgetResults = () => {
   return (
     <main>
       <h1>BudgetResults Page</h1>
-      
       <UnweightedBudget />
       <BudgetSlider />
       {/* <BudgetBtns /> */}
--- thepeoplespurse/client/src/utils/API.js
+++ thepeoplespurse/client/src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default {
-    getAgencyInfo: function(agencyCode){
+    getAgencyInfo: function (agencyCode) {
         return axios.get("https://api.usaspending.gov/api/v2/agency/" + agencyCode + "/")
-    };
+    },
 
-    getAgencyBudget: function(agencyCode) {
+    getAgencyBudget: function (agencyCode) {
         return axios.get("https://api.usaspending.gov/api/v2/agency/" + agencyCode + "/budget_function/")
     }
 };
