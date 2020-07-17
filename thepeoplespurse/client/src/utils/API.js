99a688accdbb06390515c42c513376661bc49eec
Samuel Thompson <thomps9012@gmail.com>
2020-07-16 20:34:24 -0400

    removed service worker

--- thepeoplespurse/client/src/utils/API.js
+++ thepeoplespurse/client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
     getAgencyInfo: function(agencyCode){
         return axios.get("https://api.usaspending.gov/api/v2/agency/" + agencyCode + "/")
-    };
+    },
 
     getAgencyBudget: function(agencyCode) {
         return axios.get("https://api.usaspending.gov/api/v2/agency/" + agencyCode + "/budget_function/")
